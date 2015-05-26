d3af44a79d58cea8f9ea502709310ab27dda82b5
arufian <arufian703@gmail.com>
2015-05-26 19:26:19 +0900

    changed implementation of window-button to be more natural

--- js/boilerplate.js
+++ js/boilerplate.js
@@ -1,10 +1,25 @@
 var gui = require('nw.gui');
 var appWindow = gui.Window.get();
 
+// Menu
+var nativeMenuBar = new gui.Menu({ type: "menubar" });
+nativeMenuBar.createMacBuiltin("My App");
+appWindow.menu = nativeMenuBar;
+
 // Button
-var closeBtn  = document.querySelector('#close-btn');
+var buttons = document.querySelector('.buttons');
+var buttonsLabel = document.querySelector('.buttons-label');
+var hoverIn = function(e) {
+  buttonsLabel.style.visibility = 'visible';
+}
+var hoverOut = function(e) {
+  buttonsLabel.style.visibility = 'hidden';
+}
+
+var closeBtn  = document.getElementById('close-btn');
 closeBtn.addEventListener('click', function(e) {
-  window.close();
+  console.log('close');
+  appWindow.close({force: 'true'}); 
 }, false);
 
 var maxBtn  = document.querySelector('#maximize-btn');
@@ -17,7 +32,5 @@ minBtn.addEventListener('click', function(e) {
   appWindow.minimize();
 }, false);
 
-// Menu
-var nativeMenuBar = new gui.Menu({ type: "menubar" });
-nativeMenuBar.createMacBuiltin("My App");
-appWindow.menu = nativeMenuBar;
+appWindow.show();
+appWindow.focus();
